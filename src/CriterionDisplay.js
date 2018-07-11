70cf15828c10066b136f388202b894e42dc68f44
Nathan Wilson <nathan@masschallenge.org>
2018-07-11 14:52:15 +0000

    [AC-5626] Add key details from the criterion analysis

--- src/CriterionDisplay.js
+++ src/CriterionDisplay.js
@@ -4,8 +4,15 @@ import { analyzeJudgingRoundUrl } from './utils'
 
 class CriterionDisplay extends React.Component {
     render() {
-	const analysis = this.props.analysis
-	return <div>{ analysis.criterion_name }: { analysis.option }</div>
+        const analysis = this.props.analysis
+        return (
+            <div>
+                { analysis.criterion_name } { analysis.option }:
+            Status: { analysis.completed_required_reads } of {analysis.total_required_reads},
+            Need: { analysis.remaining_needed_reads },
+            Available: { analysis.remaining_commitments },
+            Buffer: { analysis.remaining_commitments - analysis.remaining_needed_reads }
+            </div>)
     }
 }
 
