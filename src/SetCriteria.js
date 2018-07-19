569bc2e309fdc6808ec71fe2581d65018e523a6b
Jon Kiparsky <jon@kiparsky.net>
2018-07-19 14:27:23 -0400

    [AC-5624] Add header row

--- src/SetCriteria.js
+++ src/SetCriteria.js
@@ -8,7 +8,7 @@ class SetCriteria extends React.Component {
 		{"criterion_option_spec_id": 1,
 		 "criterion_option": "Just exists",
 		 "criterion_name": "exists",
-		 "weight": 10,
+		 "weight": 60,
 		 "count": 4,
 		 "need": 1234,
 		 "available": 2345,
@@ -16,7 +16,7 @@ class SetCriteria extends React.Component {
 		{"criterion_option_spec_id": 2,
 		 "criterion_option": "10EEE",
 		 "criterion_name": "shoe_size",
-		 "weight": 10,
+		 "weight": 40,
 		 "count": 1,
 		 "need": 1234,
 		 "available": 1432,
@@ -24,7 +24,7 @@ class SetCriteria extends React.Component {
 		{"criterion_option_spec_id": 3,
 		 "criterion_option": "9",
 		 "criterion_name": "shoe_size",
-		 "weight": 10,
+		 "weight": 40,
 		 "count": 1,
 		 "need": 1234,
 		 "available": 1637,
@@ -73,6 +73,29 @@ class SetCriteria extends React.Component {
 	}
 		
 	return (<table>
+		<thead>
+		<tr>
+		<td>
+		Criterion
+		</td>
+		<td>
+		Weight
+		</td>
+		<td>
+		Min Reads
+		</td>
+		<td>
+		Need
+		</td>
+		<td>
+		Available
+		</td>
+		<td>
+		Buffer
+		</td>
+		
+		</tr>
+		</thead>
 		<tbody>
 
 		{rows}
